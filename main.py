6e075274524e65169674a35a84a0cf3ed02b7f39
schneeka-22 <120665285+schneeka-22@users.noreply.github.com>
2022-12-23 10:40:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
 import csv
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 # ------------------------  function to read a csv file ----------------------------------
 def readcsv():
@@ -103,8 +105,67 @@ def delete():
         writer.writerows(lines)
         print('deleted')
 
+df = pd.read_csv('https://raw.githubusercontent.com/schneeka-22/cs-school/master/brainstoke.csv')
+
+def view_age_ar():
+
+    sns.kdeplot(x=df.loc[df['stroke'] == 1]['age'], color='#1a759f', label='Stroke',fill=True)  # age grp graph probability distribution for stroke
+    sns.kdeplot(x=df.loc[df['stroke'] == 0]['age'], color='#52b69a', label='healthy', fill=True)
+    plt.legend(loc='upper left')
+    plt.show()
+
+def age_hist():
 
 
+    x = np.arange(len(df))
+    ages = df['age']
+    binz = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+    labels = ['Children and teens', 'adults', 'middle age', 'elderly']
+    freq, bin, patches = plt.hist(ages, bins=binz, edgecolor="black")
+
+    print(freq)
+    print(bin)
+    print(patches)
+
+    for i in range(0, 2):
+        patches[i].set_facecolor('#184e77')
+    for i in range(2, 4):
+        patches[i].set_facecolor('#1a759f')
+    for i in range(4, 6):
+        patches[i].set_facecolor('#52b69a')
+    for i in range(6, 8):
+        patches[i].set_facecolor('#99d98c')
+    for i in range(8, 9):
+        patches[i].set_facecolor('#d9ed92')
+
+    colors = ['#184e77', '#1a759f', '#52b69a', '#99d98c', '#d9ed92']
+
+    font1 = {'family': 'serif', 'color': 'crimson', 'size': 20}
+    font2 = {'family': 'serif', 'color': 'darkgreen', 'size': 15}
+
+    plt.title("Age distribution of people who suffered from a stroke", fontdict=font1, loc="center")
+    plt.xlabel("age in years", fontdict=font2)
+    plt.ylabel("Number of people", fontdict=font2)
+    from matplotlib.patches import Rectangle
+    handles = [Rectangle((0, 0), 1, 1, color=c, ec='k') for c in colors]
+    plt.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, -0.08), fancybox=True, shadow=True, ncol=5)
+    plt.show()
+
+def pie_smoke():
+    status = ['Never smoked', 'Formally Smoked', 'Smokes', 'Unknown']
+
+    sizes = [144.684, 98.172, 67.176, 49.968]
+    font1 = {'family': 'serif', 'color': 'crimson', 'size': 20}
+    plt.title('Smoking status distribution', fontdict=font1)
+    colors = ['#184e77', '#1a759f', '#52b69a', '#99d98c', '#d9ed92']
+
+    plt.pie(sizes, labels=status, colors=colors, startangle=90, shadow=True, explode=(0, 0, 0.1, 0), radius=1.2,
+            autopct='%1.1f%%', textprops={'color': '#f35b04', 'font': 'serif', 'fontsize': 15})
+
+
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.08), fancybox=True, shadow=True, ncol=5)
+
+    plt.show()
 
 while(1):
     print("----------------------- MENU -----------------------")
@@ -112,10 +173,13 @@ while(1):
     print("           2. APPEND IN THE DATASET")
     print("           3. DELETE FROM THE DATASET")
     print("           4. UPDATE THE DATASET")
-    print("           5. FUNCTION 1")
-    print("           6. FUNCTION 2")
-    print("           7. FUNCTION 3")
-    print("           8. EXIT")
+    print("           5. DISPLAY STATS OF NUMERIC DATA")
+    print("           6. DISPLAY KDE PLOT FOR AGE DISTRIBUTION")
+    print("           7. DISPLAY HISTOGRAM FOR VARIOUS AGE GROUPS")
+    print("           8. DISPLAY SMOKING STATUS DISTRIBUTION")
+    print("           9. FUNC3")
+    print("           10. FUNC4")
+    print("           11. EXIT")
     ch=int(input("ENTER YOUR CHOICE : "))
     if(ch==1):
         fields = readcsv()
@@ -126,12 +190,21 @@ while(1):
     elif (ch == 4):
         updaterec()
     elif (ch == 5):
-        pass
+        print('non null value counts')
+        df.info()
+        print('stats of numeric categories')
+        print(df.describe(exclude='object'))
     elif (ch == 6):
-        pass
+        view_age_ar()
     elif (ch == 7):
-        pass
+        age_hist()
     elif (ch == 8):
+        pie_smoke()
+    elif (ch == 9):
+        break
+    elif (ch == 10):
+        break
+    elif (ch == 11):
         break
     else:
         print("INCORRECT CHOICE")
