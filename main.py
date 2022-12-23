cf7ba4067fdc125e51b71353f5475939ea12704d
schneeka-22 <120665285+schneeka-22@users.noreply.github.com>
2022-12-23 16:56:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -107,6 +107,7 @@ def delete():
 
 df = pd.read_csv('https://raw.githubusercontent.com/schneeka-22/cs-school/master/brainstoke.csv')
 
+# Which age group is most likely to suffer from a stroke?
 def view_age_ar():
 
     sns.kdeplot(x=df.loc[df['stroke'] == 1]['age'], color='#1a759f', label='Stroke',fill=True)  # age grp graph probability distribution for stroke
@@ -151,6 +152,7 @@ def age_hist():
     plt.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, -0.08), fancybox=True, shadow=True, ncol=5)
     plt.show()
 
+# What is the effect of smoking on the chances of having a stroke?
 def pie_smoke():
     status = ['Never smoked', 'Formally Smoked', 'Smokes', 'Unknown']
 
@@ -167,6 +169,49 @@ def pie_smoke():
 
     plt.show()
 
+# Does working conditions have any effect on the probability of getting a stroke
+def wrkage_str():
+    plt.title("Relation between of work type and stroke",loc="right")
+    sns.kdeplot(x=df.loc[df['stroke'] == 1]['age'], color='#184e77', label='stroke', fill=True,)
+    sns.kdeplot(x=df.loc[df['work_type'] =="Private"]['age'], color='#52b69a', label='Private', fill=True,)
+    sns.kdeplot(x=df.loc[df['work_type'] =="Govt_job" ]['age'], color='#bfd200', label='Govt_job', fill=True,)
+    sns.kdeplot(x=df.loc[df['work_type'] =="Self_employed" ]['age'], color='#34aoa4', label='Self_employed', fill=True,)
+    plt.legend(loc='upper left')
+    plt.show()
+
+# What is the influence of BMI on the possibility of a person to get a stroke?
+def bmi_str_scatter():
+  z = df['bmi']
+  y = df[df['stroke']==1]
+  x = df['age']
+  plt.title("Dependence of Stroke on BMI and Age",loc="right")
+  plt.xlabel("Age")
+  plt.ylabel("BMI")
+  plt.scatter(x,z,marker="^", color='#52b69a')
+  plt.show()
+
+# what is the probability of  a female to suffer from stroke as compared to a male of respective ages?
+
+def genage_str():
+    plt.title("Relation between Stroke, Gender and Age",loc="right")
+    sns.kdeplot(x=df.loc[df['gender'] == "Male"]['age'], color='#1a759f', label='Male',fill=True)
+    sns.kdeplot(x=df.loc[df['gender'] == "Female"]['age'], color='#52b69a', label='Female', fill=True)
+    sns.kdeplot(x=df.loc[df['stroke'] == 1]['age'], color='#184e77', label='Stroke', fill=True,)
+    plt.legend(loc='upper left')
+    plt.show()
+
+# For which range of BMI the stroke probability is greatest, what should be an ideal BMI of a person to stay away from stroke?
+
+def bmi_str_line():
+    y = df['stroke']
+    x = df['bmi']
+    plt.title("Relation between stroke and BMI", loc="right")
+    plt.xlabel('BMI', fontsize=18)
+    plt.ylabel('stroke', fontsize=12)
+    plt.bar(x, y, color='#52b69a')
+    plt.show()
+
+
 while(1):
     print("----------------------- MENU -----------------------")
     print("           1. DISPLAY THE DATASET")
@@ -175,11 +220,13 @@ while(1):
     print("           4. UPDATE THE DATASET")
     print("           5. DISPLAY STATS OF NUMERIC DATA")
     print("           6. DISPLAY KDE PLOT FOR AGE DISTRIBUTION")
-    print("           7. DISPLAY HISTOGRAM FOR VARIOUS AGE GROUPS")
-    print("           8. DISPLAY SMOKING STATUS DISTRIBUTION")
-    print("           9. FUNC3")
-    print("           10. FUNC4")
-    print("           11. EXIT")
+    print("           7. DISPLAY HISTOGRAM FOR VARIOUS AGE GROUPS WHO SUFFERED FROM A STROKE")
+    print("           8. DISPLAY SMOKING STATUS DISTRIBUTION FOR STROKE PATIENTS")
+    print("           9. SHOW RELATION BETWEEN WORK TYPE,AGE AND STROKE")
+    print("           10. SCATTER PLOT FOR DEPENDENCE OF STROKE ON BMI AND AGE")
+    print("           11. RELATION BETWEEN STROKE AND BOTH GENDERS OF RESPECTIVE AGES ")
+    print("           12. RELATION BETWEEN BMI AND STROKE")
+    print("           13. EXIT")
     ch=int(input("ENTER YOUR CHOICE : "))
     if(ch==1):
         fields = readcsv()
@@ -201,10 +248,16 @@ while(1):
     elif (ch == 8):
         pie_smoke()
     elif (ch == 9):
-        break
+        wrkage_str()
+
     elif (ch == 10):
-        break
+        bmi_str_scatter()
     elif (ch == 11):
+        genage_str()
+    elif (ch == 12):
+        bmi_str_line()
+    elif (ch == 13):
+        print ('thank you')
         break
     else:
         print("INCORRECT CHOICE")
